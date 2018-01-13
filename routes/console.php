77ba4c087a7de5d7340e67620e2bd45db47e76cc
Ely Haughie <elycin2@gmail.com>
2018-01-13 02:10:15 -0500

    Adding Clean Files cron and command

--- routes/console.php
+++ routes/console.php
@@ -11,6 +11,9 @@
 |
 */
 
+use App\Upload;
+use Carbon\Carbon;
+
 Artisan::command('make:user {name} {username} {password}', function ($name, $username, $password) {
     $new_user = \App\User::create([
         "name" => $name,
@@ -20,3 +23,14 @@ Artisan::command('make:user {name} {username} {password}', function ($name, $use
 
     echo sprintf("Created: %s\n", $new_user);
 })->describe('Create a user account');
+
+Artisan::command('clean:files', function () {
+    // Delete files older than the defined number of days
+    $num_of_days = config('app.days_to_store', 30);
+    $expiration_date = Carbon::now()->subDays($num_of_days);
+    Upload::where('created_at', '>=', $expiration_date->toDateTimeString())->delete();
+
+    //Iterate over each resolver and find where 0 uploads are.
+    $resolvers = \App\FileResolver::all();
+    foreach ($resolvers as $resolver) if (Upload::where('resolver_id', $resolver->id)->get()->count() == 0) $resolver->delete();
+})->describe('Deletes old files that are no longer being used.');
