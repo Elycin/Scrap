1befa3456e3f45f791788a70d7fb0240537baffb
Resi Respati <resir014@gmail.com>
2018-03-10 21:25:53 +0700

    removed all of the laravel frontend cruft

--- resources/assets/js/app.js
+++ resources/assets/js/app.js
@@ -5,18 +5,8 @@
  * building robust, powerful web applications using Vue and Laravel.
  */
 
-require('./bootstrap');
+require('./bootstrap')
 
-window.Vue = require('vue');
-
-/**
- * Next, we will create a fresh Vue application instance and attach it to
- * the page. Then, you may begin adding components to this application
- * or customize the JavaScript scaffolding to fit your unique needs.
- */
-
-Vue.component('example-component', require('./components/ExampleComponent.vue'));
-
-const app = new Vue({
-    el: '#app'
-});
+window.addEventListener('load', () => {
+    document.getElementById('app').innerHTML = 'yawn'
+})
